bcf941bb65d6a687b003b61186c546fd54667612
realize <KotDeg2004@gmail.com>
2018-04-04 16:22:43 +0300

    calculator v1

---
+++ calculator.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2018-04-04T16:11:54
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = calculator
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a, b;
+    cout<<"Write first number: ";
+    cin>>a;
+    cout<<"Write second number: ";
+    cin>>b;
+    cout<<"amount: "<<a+b<<endl;
+    cout<<"subtraction: "<<a-b<<endl;
+    cout<<"multiplication: "<<a*b<<endl;
+    cout<<"division: "<<a/b<<endl;
+    return 0;
+}
